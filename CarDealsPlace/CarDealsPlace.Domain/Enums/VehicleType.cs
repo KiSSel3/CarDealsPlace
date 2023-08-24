6b32aec65418acdd43b1f9196d74d903ce7b8ef6
KiSSel3 <andreyk042004@gmail.com>
2023-08-24 16:39:04 +0300

    Added models

---
+++ CarDealsPlace/CarDealsPlace.Domain/Enums/VehicleType.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDealsPlace.Domain.Enums
+{
+    public enum VehicleType
+    {
+        CONVERTIBLE,
+        UNIVERSAL,
+        HATCHBACK,
+        LIMOUSINE,
+        LIFTBACK,
+        ROADSTER,
+        PICKUP,
+        COUPE,
+        SEDAN,        
+        SUV,
+        VAN,
+
+        MOTORCYCLE,
+
+        ANOTHER,
+    }
+}
