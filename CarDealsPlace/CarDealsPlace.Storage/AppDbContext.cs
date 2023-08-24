e879d313040f667c39440381315fbcc123cd509b
KiSSel3 <andreyk042004@gmail.com>
2023-08-24 17:43:50 +0300

    Add AppDbContext

---
+++ CarDealsPlace/CarDealsPlace.Storage/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using CarDealsPlace.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDealsPlace.Storage
+{
+    public class AppDbContext : DbContext
+    {
+        DbSet<OfferModel> Offers { get; set; } = null!;
+        DbSet<UserModel> Users { get; set; } = null!;
+        DbSet<VehicleModel> Vehicles { get; set; } = null!;
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+
+
+        }
+    }
+}
