acb58f57f223bf6cb85857c8dec4243fb9488121
KiSSel3 <andreyk042004@gmail.com>
2023-08-24 21:17:59 +0300

    Added classes for working with the database

--- CarDealsPlace/CarDealsPlace.Storage/AppDbContext.cs
+++ CarDealsPlace/CarDealsPlace.Storage/AppDbContext.cs
@@ -5,9 +5,9 @@ namespace CarDealsPlace.Storage
 {
     public class AppDbContext : DbContext
     {
-        DbSet<OfferModel> Offers { get; set; } = null!;
-        DbSet<UserModel> Users { get; set; } = null!;
-        DbSet<VehicleModel> Vehicles { get; set; } = null!;
+        public DbSet<OfferModel> Offers { get; set; } = null!;
+        public DbSet<UserModel> Users { get; set; } = null!;
+        public DbSet<VehicleModel> Vehicles { get; set; } = null!;
 
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
