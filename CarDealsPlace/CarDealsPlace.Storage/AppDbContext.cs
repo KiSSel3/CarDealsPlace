bcca4f8dce1ad793045d81790633c57e712db56c
KiSSel3 <andreyk042004@gmail.com>
2023-08-24 18:09:49 +0300

    Cascading deletion is configured

--- CarDealsPlace/CarDealsPlace.Storage/AppDbContext.cs
+++ CarDealsPlace/CarDealsPlace.Storage/AppDbContext.cs
@@ -1,10 +1,5 @@
 ﻿using CarDealsPlace.Domain.Models;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CarDealsPlace.Storage
 {
@@ -17,8 +12,24 @@ namespace CarDealsPlace.Storage
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
             Database.EnsureCreated();
+        }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
 
+            //FIXME: каскадное удаление в бд может быть настроено некорректно
+            modelBuilder.Entity<OfferModel>()
+                .HasOne(o => o.Vehicle)
+                .WithMany()
+                .HasForeignKey(o => o.VehicleId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<OfferModel>()
+                .HasOne(o => o.User)
+                .WithMany()
+                .HasForeignKey(o => o.UserId)
+                .OnDelete(DeleteBehavior.Restrict);
         }
     }
 }
