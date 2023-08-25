96adb3a83ff3eeb1479cb7ff905c34442a8798a5
KiSSel3 <andreyk042004@gmail.com>
2023-08-25 17:48:28 +0300

    Added filling the database at creation

--- CarDealsPlace/CarDealsPlace.Storage/AppDbContext.cs
+++ CarDealsPlace/CarDealsPlace.Storage/AppDbContext.cs
@@ -18,18 +18,60 @@ namespace CarDealsPlace.Storage
         {
             base.OnModelCreating(modelBuilder);
 
+            #region Каскадно удаление (Временно отключено)
             //FIXME: каскадное удаление в бд может быть настроено некорректно
-            modelBuilder.Entity<OfferModel>()
-                .HasOne(o => o.Vehicle)
-                .WithMany()
-                .HasForeignKey(o => o.Vehicle.Id)
-                .OnDelete(DeleteBehavior.Cascade);
+            /*            modelBuilder.Entity<OfferModel>()
+                            .HasOne(o => o.Vehicle)
+                            .WithMany()
+                            .HasForeignKey(o => o.Vehicle.Id)
+                            .OnDelete(DeleteBehavior.Cascade);
 
-            modelBuilder.Entity<OfferModel>()
-                .HasOne(o => o.User)
-                .WithMany()
-                .HasForeignKey(o => o.User.Id)
-                .OnDelete(DeleteBehavior.Restrict);
+                        modelBuilder.Entity<OfferModel>()
+                            .HasOne(o => o.User)
+                            .WithMany()
+                            .HasForeignKey(o => o.User.Id)
+                            .OnDelete(DeleteBehavior.Restrict);*/
+            #endregion
+
+            #region Заполнение бд (Временно)
+            VehicleModel vehicle = new VehicleModel()
+            {
+                Id = Guid.NewGuid(),
+                Brand = "BMW",
+                Model = "M5 f90",
+                Mileage = 5,
+                EngineDisplacement = 15,
+                ProductionYear = new DateTime(2023),
+                VehicleType = Domain.Enums.VehicleType.SEDAN,
+                WheelDriveType = Domain.Enums.WheelDriveType.ALL,
+                TransmissionType = Domain.Enums.TransmissionType.AUTOMATIC,
+                ImageUrls = new(),
+            };
+            modelBuilder.Entity<VehicleModel>().HasData(vehicle);
+
+            UserModel user = new UserModel()
+            {
+                Id = Guid.NewGuid(),
+                Login = "kissel",
+                Password = "kissel",
+                Email = "---",
+                Name = "Andrey",
+                PhoneNumber = "---",
+                ImageUrl = "---",
+            };
+            modelBuilder.Entity<UserModel>().HasData(user);
+
+            OfferModel offer = new OfferModel()
+            {
+                Id = Guid.NewGuid(),
+                User = user,
+                Vehicle = vehicle,
+                PublicationData = DateTime.Now,
+                Description = "---",
+                Price = 150000.0f,
+            };
+            modelBuilder.Entity<OfferModel>().HasData(offer);
+            #endregion
         }
     }
 }
