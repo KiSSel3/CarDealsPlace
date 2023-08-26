ce5e4a6ee65138d82a287f9286d94379a581c360
KiSSel3 <andreyk042004@gmail.com>
2023-08-26 14:38:29 +0300

    Objects in the database were created, NuGet package version 1.1.0 was created.

--- CarDealsPlace/CarDealsPlace.Storage/AppDbContext.cs
+++ CarDealsPlace/CarDealsPlace.Storage/AppDbContext.cs
@@ -18,60 +18,18 @@ namespace CarDealsPlace.Storage
         {
             base.OnModelCreating(modelBuilder);
 
+            //FIXME: Если возникла проблема с удалением объекта в бд, ошибка тут
             modelBuilder.Entity<OfferModel>()
-                 .HasOne(o => o.Vehicle)
-                 .WithMany()
-                 .HasForeignKey(o => o.UserId)
-                 .OnDelete(DeleteBehavior.Cascade);
-
-            modelBuilder.Entity<OfferModel>()
-                .HasOne(o => o.User)
+                .HasOne<UserModel>(o => o.User)
                 .WithMany()
                 .HasForeignKey(o => o.UserId)
-                .OnDelete(DeleteBehavior.Restrict);
-            
+                .OnDelete(DeleteBehavior.Restrict);//Тут
 
-            #region Заполнение бд (Временно)
-VehicleModel vehicle = new VehicleModel()
-{
-    Id = Guid.NewGuid(),
-    Brand = "BMW",
-    Model = "M5 f90",
-    Mileage = 5,
-    EngineDisplacement = 15,
-    ProductionYear = new DateTime(2023),
-    VehicleType = Domain.Enums.VehicleType.SEDAN,
-    WheelDriveType = Domain.Enums.WheelDriveType.ALL,
-    TransmissionType = Domain.Enums.TransmissionType.AUTOMATIC,
-    ImageUrls = new(),
-};
-            modelBuilder.Entity<VehicleModel>().HasData(vehicle);
-
-            UserModel user = new UserModel()
-            {
-                Id = Guid.NewGuid(),
-                Login = "kissel",
-                Password = "kissel",
-                Email = "---",
-                Name = "Andrey",
-                PhoneNumber = "---",
-                ImageUrl = "---",
-            };
-            modelBuilder.Entity<UserModel>().HasData(user);
-
-            OfferModel offer = new OfferModel()
-            {
-                Id = Guid.NewGuid(),
-                User = user,
-                UserId = user.Id,
-                Vehicle = vehicle,
-                VehicleId = vehicle.Id,
-                PublicationData = DateTime.Now,
-                Description = "---",
-                Price = 150000.0f,
-            };
-            modelBuilder.Entity<OfferModel>().HasData(offer);
-            #endregion
+            modelBuilder.Entity<OfferModel>()
+                .HasOne<VehicleModel>(o => o.Vehicle)
+                .WithMany()
+                .HasForeignKey(o => o.VehicleId)
+                .OnDelete(DeleteBehavior.Cascade);//Тут
         }
     }
 }
